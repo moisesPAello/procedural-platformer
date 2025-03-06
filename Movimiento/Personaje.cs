1d8e93db420568685478b434bccbd1dab3ea9f1c
Genar0Islas <a223215520@unison.mx>
2025-03-06 01:55:41 -0700

    Clase movimiento y clase personaje

---
+++ Movimiento/Personaje.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace PersonajeEjemplo
+{
+    public class Personaje
+    {
+        public Vector2 Posicion { get; set; }
+        public int Velocidad { get; set; }
+
+        public Personaje(Vector2 posicionInicial, int velocidad)
+        {
+            Posicion = posicionInicial;
+            Velocidad = velocidad;
+        }
+    }
+}
